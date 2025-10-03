8bce365ea64249ebc3d587a45f5d1ca2cd6e55cd
luis267 <105466531+luis267@users.noreply.github.com>
2025-10-03 23:49:09 +0000

    hey

---
+++ projeto/src/main/java/com/example/projeto/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.example.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.projeto.model.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+}
