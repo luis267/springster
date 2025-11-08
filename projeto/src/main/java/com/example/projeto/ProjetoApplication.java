7b3c94469b9c7f3a8201c07755d7213c37dc912c
luis267 <105466531+luis267@users.noreply.github.com>
2025-11-08 01:25:43 +0000

    ou

--- projeto/src/main/java/com/example/projeto/ProjetoApplication.java
+++ projeto/src/main/java/com/example/projeto/ProjetoApplication.java
@@ -3,7 +3,10 @@ package com.example.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.projeto") // <-- ADICIONE ISSO
+// Apenas @SpringBootApplication, sem atributos extras.
+// Isso garantirá que o Spring escaneie todos os sub-pacotes
+// (como .config, .controller, .service, .repository)
+@SpringBootApplication
 public class ProjetoApplication {
 
 	public static void main(String[] args) {
