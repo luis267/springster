b9a05639f8dd52703cb2365acc3040f5fa0049b5
luis267 <105466531+luis267@users.noreply.github.com>
2025-11-24 21:34:46 +0000

    atividade feita

--- projeto/src/main/java/com/example/projeto/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projeto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.projeto.model.Pessoa;
-
-@Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
